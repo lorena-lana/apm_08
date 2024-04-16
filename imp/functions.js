5b3c41c6d46f8103cf60cbc46dba25ca53e0d8be
lorena <allana.rocha.delorena@gmail.com>
2024-04-16 16:33:31 -0300

    Atualização 16/04

---
+++ imp/functions.js
@@ -0,0 +1,12 @@
+function response (sts, msg, aftrows, data=null){
+    return{
+        status:sts,
+        message:msg,
+        affected_rows:aftrows,
+        data:data,
+        timestamp:new Date().getTime()
+    }
+}
+module.exports = {
+    response
+}
